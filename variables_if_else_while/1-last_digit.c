7204f7743a3d46103aac5500cae81c74b40637d0
root <7457@holbertonstudents.com>
2023-10-16 08:00:00 -0700

    exo1 update

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - launch program to say if positif or negative
- * Return: zero
+ * Return: 0
  */
 int main(void)
 {
@@ -20,11 +20,15 @@ int main(void)
 
 	if (last_digit > 5)
 	{
-		printf ("and is greater than 5\n");
+		printf ("Last digit of %d is %d and is greater than 5\n",n, last_digit);
 	}
 	if (last_digit == 0)
 	{
-		printf ("and is 0\n");
+		printf ("Last digit of %d is %d and is 0\n",n, last_digit);
 	}
-	if (last_digit < 6 && !=0)        
+	if (last_digit < 6 && !=0)
+	{
+		printf ("Last digit of %d is %d and is less than 6 and not 0\n",n, last_digit);
+	}
+	return (0);
 }
