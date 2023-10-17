48e8655329989d90a96ee0a6a2ce0bd04b090653
root <7457@holbertonstudents.com>
2023-10-17 05:11:50 -0700

    print numbers 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - program to prints all single digit numbers of 10 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+	printf("%d", num);
+	}
+
+return (0);
+}
