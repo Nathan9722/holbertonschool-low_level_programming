ca07eea8b2706d7103570cbccf0465223e469cf2
root <7457@holbertonstudents.com>
2023-10-17 02:30:49 -0700

    adding script who remove q and e in alphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - launch program to print the alphabet and avoid q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+	return (0);
+}
