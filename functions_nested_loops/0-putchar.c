354079b0a2d87a87abccad5a9f42b1d1d02f3e08
root <7457@holbertonstudents.com>
2023-10-18 05:23:04 -0700

    nested loops

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - launch program to print the alphabet and avoid q and e
@@ -6,3 +6,9 @@
  */
 
 int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+}
+
