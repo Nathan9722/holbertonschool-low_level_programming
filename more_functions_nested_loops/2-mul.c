37002694d1964b13990dbdcc8298c41f434fa4b7
root <7457@holbertonstudents.com>
2023-10-22 04:11:07 -0700

    adding exo 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - write a function that multiples 2 integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 'a' * 'b'
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,4 +7,5 @@ int main(void);
 int _putchar(char c);
 int _isdigit(int c);
 int _isupper(int c);
+int mul(int a, int b);
 #endif
