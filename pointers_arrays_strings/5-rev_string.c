405072cf7393ee687de2f390c6bf7d47326ce7a5
root <7457@holbertonstudents.com>
2023-10-26 01:28:27 -0700

    rev

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void rev_string(char *s)
+{
+	int str = strlen(s);
+	int i, j;
+	char temp;
+
+	for (i = 0, j = str - 1; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
