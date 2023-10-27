357b032a46c4669f731ef133855c3c9f7c645288
root <7457@holbertonstudents.com>
2023-10-27 06:36:15 -0700

    test exo 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 /**
  * print_array - prints n elements of an array of integers
- *
+ * @a: pointer
+ * @n: the number of elements of the array to be printed
  */
 void print_array(int *a, int n)
 {
