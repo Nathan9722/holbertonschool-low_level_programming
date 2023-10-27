bdac9265c6c5dcee2f404ecf72640610e1f370c0
root <7457@holbertonstudents.com>
2023-10-27 04:12:59 -0700

    exo 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ * @i: is the value of index
+ *
+ */
+void puts_half(char *str)
+{
+	int strl = strlen(str);
+
+	int i;
+
+	if (strl % 2 == 0)
+	{
+		i = strl / 2;
+	}
+	else
+	{
+		i = (strl - 1) / 2;
+	}
+	for (; i < strl; i++)
+	{
+		_putchar(str[i])
+	}
+	_putchar(str[i]);
+}
