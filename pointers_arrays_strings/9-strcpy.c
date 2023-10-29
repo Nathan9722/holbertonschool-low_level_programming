67c96db37c231fd6a7abe0a50d16f18d89b60b68
root <7457@holbertonstudents.com>
2023-10-29 03:23:23 -0700

    exo 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function that copies the string pointed to by src
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int j, i;
+
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i++)
+	{
+		dest[i] = src [i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
