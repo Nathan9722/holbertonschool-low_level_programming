e5d7ba51cba17448185e90cab34f6c6eac443854
root <7457@holbertonstudents.com>
2023-11-01 15:34:45 -0700

    exo 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,12 +4,13 @@
  * _strlen_recursion - function that returns the length of a string
  *
  * @s: string
+ *
+ * Return: 0
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		return (0);
-		_putchar(*s);
-	}
+	_putchar(*s);
+	return (1 + _strlen_recursion(s + 1));
 }
