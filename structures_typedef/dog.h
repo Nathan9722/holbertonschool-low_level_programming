80c90bfed58b5a9c9c583ce013c06f75391e0ca6
Nathan9722 <146355716+Nathan9722@users.noreply.github.com>
2023-11-14 11:11:34 +0100

    adding exo 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - structure with basic info name, age, owner
+ * @name: name of the user
+ * @age: age of the user
+ * @owner: owner
+ * 
+*/
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
